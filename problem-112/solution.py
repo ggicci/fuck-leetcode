f147a408b2150d084dffd5747723502c67a9149a
Ggicci <ggicci.t@gmail.com>
2019-11-11 01:45:46 +0800

    refactor: add some meta info for problem 112's solution

--- problem-112/solution.py
+++ problem-112/solution.py
@@ -1,3 +1,13 @@
+# Leetcode Problem #112 - Path Sum
+# https://leetcode.com/problems/path-sum/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution:
 
     def hasPathSum(self, root: TreeNode, sum: int) -> bool:
